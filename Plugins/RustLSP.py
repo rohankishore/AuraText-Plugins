32103f83d578aecf027274a4621bf8849919abb4
Matthew Yang <matthew.yang204@gmail.com>
2026-06-26 10:58:21 -0700

    Add a RustLSP plugin

---
+++ Plugins/RustLSP.py
@@ -0,0 +1,59 @@
+import os
+import platform
+import urllib.request
+import io
+import zipfile
+
+from PyQt6.QtCore import QTimer
+from PyQt6.QtWidgets import QMessageBox, QProgressDialog
+from auratext import Plugin
+
+class RustLSP(Plugin):
+    def __init__(self, window):
+        super().__init__(window)
+        self.window = window
+
+        if platform.system() == "Windows":
+            self.binaryPath = os.path.join(self.window.local_app_data, "bin", "rust-analyzer.exe")
+        else:
+            self.binaryPath = os.path.join(self.window.local_app_data, "bin", "rust-analyzer")
+
+        if os.path.exists(self.binaryPath):
+            print("INFO: Rust-analyzer already on disk, adding to global LSP list...")
+            self.window.registerLSPDictEntry("rust", self.binaryPath)
+        else:
+            print("INFO: Loading rust-analyzer from the internet as it is not on disk...")
+            QTimer.singleShot(0, self.loadFromInternet)
+
+    def download(self, url):
+        response = urllib.request.urlopen(url)
+        filedata = response.read()
+        response.close()
+        return filedata
+
+    def loadFromInternet(self):
+        if platform.system() == "Windows":
+            url = "https://github.com/rust-lang/rust-analyzer/releases/download/2026-06-22/rust-analyzer-x86_64-pc-windows-msvc.zip"
+            compression = "zip"
+            filename = "rust-analyzer.exe"
+
+        print("INFO: Downloading binary...")
+        compressedData = self.download(url)
+        print("INFO: Decompressing payload...")
+        if compression == "zip":
+            zip_bytes = io.BytesIO(compressedData)
+            with zipfile.ZipFile(zip_bytes) as z:
+                for name in z.namelist():
+                    if name.endswith(filename):
+                        binary_data = z.read(name)
+                        break
+                else:
+                    raise RuntimeError("Binary not found in ZIP")
+        print("INFO: Writing rust-analyzer to disk...")
+        binDir = os.path.dirname(self.binaryPath)
+        os.makedirs(binDir, exist_ok=True)
+        with open(self.binaryPath, "wb") as f:
+            f.write(binary_data)
+
+        print("INFO: Registering rust-analyzer into global LSP dictionary...")
+        self.window.registerLSPDictEntry("rust", self.binaryPath)
